df6c0b2c0f1f34d6e1a0daef0c8316a34987e050
silversky9 <nopphadon.phanwong@gmail.com>
2022-05-10 14:52:52 +0700

    feat: Add init SQL Statement for List faculty

--- db.sql
+++ db.sql
@@ -1,7 +1,13 @@
-CREATE TABLE Persons (
-    PersonID int,
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
+CREATE TABLE faculty (
+    faculty_id int,
+    faculty_name varchar(255)
 );
+
+INSERT INTO faculty (faculty_id, faculty_name)
+VALUES (1, 'วิศวกรรมศาสตร์');
+INSERT INTO faculty (faculty_id, faculty_name)
+VALUES (2, 'วิทยาศาสตร์');
+INSERT INTO faculty (faculty_id, faculty_name)
+VALUES (3, 'สถาปัตยกรรมศาสตร์');
+INSERT INTO faculty (faculty_id, faculty_name)
+VALUES (4, 'เทคโนโลยีสารสนเทศ');
