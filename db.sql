236568228af796506bcc405436a213d1ba460387
silversky9 <nopphadon.phanwong@gmail.com>
2022-05-10 15:10:51 +0700

    feat: Can get list of faculty name

--- db.sql
+++ db.sql
@@ -6,8 +6,26 @@ CREATE TABLE faculty (
 INSERT INTO faculty (faculty_id, faculty_name)
 VALUES (1, 'วิศวกรรมศาสตร์');
 INSERT INTO faculty (faculty_id, faculty_name)
-VALUES (2, 'วิทยาศาสตร์');
+VALUES (2, 'บริหาร');
 INSERT INTO faculty (faculty_id, faculty_name)
-VALUES (3, 'สถาปัตยกรรมศาสตร์');
+VALUES (3, 'ทัตแพทยศาสตร์');
 INSERT INTO faculty (faculty_id, faculty_name)
-VALUES (4, 'เทคโนโลยีสารสนเทศ');
+VALUES (4, 'อุสาหกรรมการบิน');
+INSERT INTO faculty (faculty_id, faculty_name)
+VALUES (5, 'สถาปัตยกรรมศาสตร์');
+INSERT INTO faculty (faculty_id, faculty_name)
+VALUES (6, 'นวัตกรรมขั้นสูง');
+
+
+CREATE TABLE round_ava (
+    round_id int,
+    round_name varchar(255)
+);
+INSERT INTO round_ava (round_id, round_name)
+VALUES (1, '(1/2565) รอบ Dircet Admisstion 3 (international Program)');
+INSERT INTO round_ava (round_id, round_name)
+VALUES (2, '(1/2565) รอบ Dircet Admisstion 2 (international student Program)');
+INSERT INTO round_ava (round_id, round_name)
+VALUES (3, '(1/2565) รอบ Dircet Admisstion 2 (international Program)');
+INSERT INTO round_ava (round_id, round_name)
+VALUES (4, '(1/2565) รอบ 2 โครงการอาชีวะพรีเมี่ยม');
